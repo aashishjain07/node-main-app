f283080cdafe9364b58700fec3ebf46c7a2f87e8
Ashish Jain <Ashish.jain8847@gmail.com>
2018-05-17 09:52:17 +0530

    Test GET Todo/:id

--- server/server.js
+++ server/server.js
@@ -1,6 +1,6 @@
-const {ObjectID} = require('mongodb');
 var express = require('express');
 var bodyParser = require('body-parser');
+var {ObjectID} = require('mongodb');
 
 var {mongoose} = require('./db/mongoose');
 var {Todo} = require('./models/todo');
@@ -11,10 +11,10 @@ var app = express();
 app.use(bodyParser.json());
 
 app.post('/todos', (req, res) => {
-  //console.log(req.body);
-  var todo = new Todo ({
+  var todo = new Todo({
     text: req.body.text
   });
+
   todo.save().then((doc) => {
     res.send(doc);
   }, (e) => {
@@ -24,7 +24,7 @@ app.post('/todos', (req, res) => {
 
 app.get('/todos', (req, res) => {
   Todo.find().then((todos) => {
-      res.send({todos});
+    res.send({todos});
   }, (e) => {
     res.status(400).send(e);
   });
@@ -32,23 +32,24 @@ app.get('/todos', (req, res) => {
 
 app.get('/todos/:id', (req, res) => {
   var id = req.params.id;
-  //res.send(req.params);
-  if(!ObjectID.isValid(id)) {
-    return res.status(404).send();
-  }
-  Todo.findById(id).then((todo) => {
-    if(!todo) {
+
+  if (!ObjectID.isValid(id)) {
     return res.status(404).send();
   }
 
-  res.send({todo});
+  Todo.findById(id).then((todo) => {
+    if (!todo) {
+      return res.status(404).send();
+    }
+
+    res.send({todo});
   }).catch((e) => {
     res.status(400).send();
   });
 });
 
 app.listen(3000, () => {
-  console.log('Starting on port 3000');
+  console.log('Started on port 3000');
 });
 
 module.exports = {app};
